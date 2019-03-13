848ee5b8e8f44f40c1e85c7ec2c7593ece3a4f3e
PaulClas <paul.clas@polymtl.ca>
2019-03-12 23:46:31 -0400

    Modif final pour remise

--- tp3/code_source_fourni/code_source_fourni/BinarySearchTree.java
+++ tp3/code_source_fourni/code_source_fourni/BinarySearchTree.java
@@ -1,22 +1,41 @@
+/*
+ * Paul Clas 1846912 & Mazigh Ouanes 1921035
+ * 12 Mars 2019
+ * BinarySearchTree.java
+ */
+package tp3;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-
+/**
+ * 
+ * @author User
+ *
+ * @param <T>
+ */
 public class BinarySearchTree<T extends Comparable<? super T> > {
 
     private BinaryNode<T> root;
 
+    /*
+     * 
+     */
     public BinarySearchTree() { }
 
     // TODO: initialisation
+    /*
+     * 
+     */
     public BinarySearchTree(T item) {
+    	root = new BinaryNode<T>(item);
 
     }
 
     // TODO: on insere un nouvel item a partir de la racine
     // O(log(n))
     public void insert(T item) {
+    	root.insert(item);
 
     }
 
@@ -24,24 +43,37 @@ public class BinarySearchTree<T extends Comparable<? super T> > {
     // O(log(n))
     public boolean contains(T item) {
 
-        return false;
+        return root.contains(item);
     }
 
     // TODO: trouver la hauteur de l'arbre
     // O(n)
     public int getHeight() {
-        return -1;
+        return root.getHeight();
     }
 
     // TODO: placer dans une liste les items de l'arbre en ordre
     // O(n)
     public List<BinaryNode<T>> getItemsInOrder() {
-        return null;
+        List<BinaryNode<T>> list = new ArrayList<BinaryNode<T>>();
+        root.fillListInOrder(list);
+        return list;
     }
 
     // TODO: retourner la liste d'item en String selon le bon format
     // O(n)
     public String toStringInOrder() {
-        return "[1, 2, 3]";
+    	String str = "[ "; //ne semble pas marcher
+    	List<BinaryNode<T>> list =getItemsInOrder();
+    	for(int i=0; i<list.size();++i) {
+    		str+= list.get(i).getData().toString(); // list.getData(i).toString() ???
+    		if(i!=list.size()-1) {
+    			str+= " , ";
+    		}
+    		//str+=" ]";
+    	}
+    	str+=" ]";
+    	
+        return str;
     }
 }
--- tp3/code_source_fourni/code_source_fourni/CompanyTree.java
+++ tp3/code_source_fourni/code_source_fourni/CompanyTree.java
@@ -1,3 +1,9 @@
+/*
+ * Paul Clas 1846912 & Mazigh Ouanes 1921035
+ * 12 Mars 2019
+ * CompanyTree.java
+ */
+package tp3;
 public class CompanyTree {
 
     private CompanyNode root;
@@ -6,33 +12,37 @@ public class CompanyTree {
 
     // TODO: initialisation
     public CompanyTree(CompanyNode item) {
+    	root =item;
 
     }
 
     // TODO: une compagnie mère achete une autre compagnie
     // O(n)
     public void buy(CompanyNode item) {
+    	root.buy(item);
 
     }
 
     // TODO: on retourne le montant en banque de la compagnie mère
     // O(1)
     public Integer getMoney() {
-        return Integer.MIN_VALUE;
+        return root.getMoney();
     }
 
     // TODO: si on avait a laisser tomber un enfant, ça serait lui
     // Ceci est le pire de tous les enfants et les sous-enfants
     // O(1)
     public Integer getWorstChildMoney() {
-        return Integer.MIN_VALUE;
+        return root.worstChild.getMoney();
     }
 
     // TODO: on retourne en string un presentation en ordre inverse
     // de la compagnie mère et de ses enfants
     // O(1)
     public String getTreeInOrder() {
-
-        return "";
+    	StringBuilder str = new StringBuilder();
+    	String prefix = " ";
+    	root.fillStringBuilderInOrder(str, prefix); // ou sinon juste faire "" a la place de prefixe
+        return str.toString();
     }
 }
