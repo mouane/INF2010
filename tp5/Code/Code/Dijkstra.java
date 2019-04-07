1a3db1cce03e6dbd7b542b20b287aac7e7f1f040
PaulClas <paul.clas@polymtl.ca>
2019-04-07 03:13:50 -0400

    unzip lab5

---
+++ tp5/Code/Code/Dijkstra.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class Dijkstra {
+
+	private Graph graph;
+	private Map<Node, Edge> dijkstraTable[];
+	private Stack<Edge> path;
+	
+
+	public Dijkstra (Graph g) {
+		this.graph = g;
+	}
+
+	public void findPath (Node s, Node d) {
+
+		dijkstraTable = new HashMap[graph.getNodes().size()];
+		path = new Stack<Edge>();
+		
+		// A compléter
+
+		
+	}
+
+	private Node getMinimum(Map<Node, Edge> map) {
+		Edge min = null;
+		for (Node Key : map.keySet()) {
+			if ( min == null || map.get(Key).getDistance() < min.getDistance()) {
+				min = map.get(Key); 
+			}
+		}
+		return min.getDestination();
+	}
+
+	private Edge getMinimum (Edge e1, Edge e2) {
+		// A completer
+	}
+	
+	public String afficherCourtChemin (Node source, Node destination) {
+		// A completer
+	}
+
+	public void afficherTable () {
+		// A completer
+		
+	}
+}
